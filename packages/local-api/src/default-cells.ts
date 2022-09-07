48a996856a499e65cce1c8ccacbd30f48e5f3c86
Joshua Lokken <joshualokken@pm.me>
2022-09-07 07:54:45 -0500

    Update default cells

--- packages/local-api/src/default-cells.ts
+++ packages/local-api/src/default-cells.ts
@@ -2,20 +2,20 @@ export const defaultCells = `
 - content: |-
     # Welcome!
 
-    Click any text cell (including this one) to start editing.  
-    You have a fully-equipped Markdown editor at your disposal.  
-    You can also create a new cell by hover above or below any cell.  
+    Click any text cell (including this one) to start editing.    
+    You have a fully-equipped Markdown editor at your disposal.    
+    You can also create a new cell by hover above or below any cell.    
 
-    https://portfolio.joshualokken.tech  
-    joshualokken@pm.me  
-    https://github.com/jrrlokken
+    https://portfolio.joshualokken.tech    
+    joshualokken@pm.me    
+    https://github.com/jrrlokken  
   type: text
   id: sxfry
 - content: |-
     import ReactDOM from 'react-dom';
 
     const App = () => {
-      fetch('https://portfolio.joshualokken.tech')
+      fetch('https://jsonplaceholder.typicode.com/todos/1')
         .then((response) => response.json())
         .then((data) => show(data));
         .catch((error) => {
